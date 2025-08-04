34b43fdad4fea06d81d576401ee3c1837a5af1ac
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-08-04 12:17:49 +0700

    Commands implementation

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/DeletePostCommand.cs
@@ -0,0 +1,8 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api;
+
+public class DeletePostCommand : BaseCommand
+{
+    public string Username { get; set; }
+}
---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/EditCommentCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Post.Cmd.Api.Commands;
+
+public class EditCommentCommand
+{
+    public Guid CommentId { get; set; }
+    public string Comment { get; set; }
+    public string Username { get; set; }
+}
